d53c80f84278992c01db3cfddb68b92363b8ce5b
vincent-duan <1621424380@qq.com>
2017-11-18 21:38:57 +0800

    update

--- file2DB/file2DB-service/src/main/java/org/dao/ExtractDao.java
+++ file2DB/file2DB-service/src/main/java/org/dao/ExtractDao.java
@@ -11,22 +11,27 @@ import java.util.List;
  * Time: 11:21
  */
 public interface ExtractDao {
-    void updateMysqlPerCf1C18(@Param("list")List<String[]> list);
-    void updateMysqlPerCf1C19(@Param("list")List<String[]> list);
-    void updateMysqlPerCf1C28(@Param("list")List<String[]> list);
-    void updateMysqlPerCf1C32(@Param("list")List<String[]> list);
-    void updateMysqlPerCf1C39(@Param("list")List<String[]> list);
-    void updateMysqlPerCf1C50(@Param("list")List<String[]> list);
+    void updateMysqlPerCf1C18(@Param("list") List<String[]> list);
 
-    void updateMysqlPerCf1C18ForOne(@Param("item")String[] cf1c18);
+    void updateMysqlPerCf1C19(@Param("list") List<String[]> list);
 
-    void updateMysqlPerCf1C19ForOne(@Param("item")String[] cf1c19);
+    void updateMysqlPerCf1C28(@Param("list") List<String[]> list);
 
-    void updateMysqlPerCf1C50ForOne(@Param("item")String[] cf1c50);
+    void updateMysqlPerCf1C32(@Param("list") List<String[]> list);
 
-    void updateMysqlPerCf1C39ForOne(@Param("item")String[] cf1c39);
+    void updateMysqlPerCf1C39(@Param("list") List<String[]> list);
 
-    void updateMysqlPerCf1C32ForOne(@Param("item")String[] cf1c32);
+    void updateMysqlPerCf1C50(@Param("list") List<String[]> list);
 
-    void updateMysqlPerCf1C28ForOne(@Param("item")String[] cf1c28);
+    void updateMysqlPerCf1C18ForOne(@Param("item") String[] cf1c18);
+
+    void updateMysqlPerCf1C19ForOne(@Param("item") String[] cf1c19);
+
+    void updateMysqlPerCf1C50ForOne(@Param("item") String[] cf1c50);
+
+    void updateMysqlPerCf1C39ForOne(@Param("item") String[] cf1c39);
+
+    void updateMysqlPerCf1C32ForOne(@Param("item") String[] cf1c32);
+
+    void updateMysqlPerCf1C28ForOne(@Param("item") String[] cf1c28);
 }
