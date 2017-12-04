c188a16fd2b43393c413f2dce4a263d3fc7e9fe2
duandingyang <2546469650@qq.com>
2017-12-04 22:50:31 +0800

    update

--- file2DB/file2DB-web/src/main/java/org/taian/web/Test.java
+++ file2DB/file2DB-web/src/main/java/org/taian/web/Test.java
@@ -25,8 +25,8 @@ import java.util.*;
 public class Test {
     public static void main(String[] args) {
         Map<String, Object> props = new HashMap<>();
-        //props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.152.45:9092");
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.16.247.130:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.152.45:9092");
+        //props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.16.247.130:9092");
         props.put(ProducerConfig.RETRIES_CONFIG, 0);
         props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
         props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
@@ -35,8 +35,8 @@ public class Test {
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         ProducerFactory<Integer, String> pf = new DefaultKafkaProducerFactory<>(props);
         KafkaTemplate<Integer, String> template = new KafkaTemplate<>(pf);
-        String fileName = "/Users/duandingyang/test/ddy.txt";
-        //String fileName = "D:/test/ddy-200.txt";
+        //String fileName = "/Users/duandingyang/test/ddy.txt";
+        String fileName = "D:/test/ddy.txt";
         File file = new File(fileName);
         //File file = new File("/root/tmp_data/fakeData/data20171024/FakeData_t3.txt");
         System.out.println("start: producer" + new DateTime(System.currentTimeMillis()));
