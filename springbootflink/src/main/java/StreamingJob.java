c75d16a73e9816a3b61f88ccf7713a70603b5350
vincent-duan <1621424380@qq.com>
2019-08-26 22:05:33 +0800

    update

---
+++ springbootflink/src/main/java/StreamingJob.java
@@ -0,0 +1,35 @@
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+import scala.Tuple2;
+
+/**
+ * @author vincent
+ * @time 2019-08-24 17:48
+ */
+public class StreamingJob {
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStream<String> text = env.socketTextStream("127.0.0.1", 9000);
+        DataStream<Tuple2<String, Integer>> dataStream = text.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception {
+                String[] tokens = s.toLowerCase().split("\\W+");
+
+                for (String token : tokens) {
+                    if (token.length() > 0) {
+                        collector.collect(new Tuple2<String, Integer>(token, 1));
+                    }
+                }
+            }
+
+
+        }).keyBy(0).timeWindow(Time.seconds(5)).sum(1);
+
+        dataStream.print();
+        // execute program
+        env.execute("Java WordCount from SocketTextStream Example");
+    }
+}
---
+++ springbootflink/src/main/java/wikiedits/WikipediaAnalysis.java
@@ -0,0 +1,11 @@
+package wikiedits;
+
+/**
+ * @author vincent
+ * @time 2019-08-26 21:45
+ */
+public class WikipediaAnalysis {
+    public static void main(String[] args) {
+
+    }
+}
