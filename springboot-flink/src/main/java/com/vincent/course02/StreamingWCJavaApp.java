748fed595c9d8ae870f083873c65e6db1650582c
duandingyang <1621424380@qq.com>
2019-09-03 19:42:52 +0800

    update

--- springboot-flink/src/main/java/com/vincent/course02/StreamingWCJavaApp.java
+++ springboot-flink/src/main/java/com/vincent/course02/StreamingWCJavaApp.java
@@ -2,7 +2,6 @@ package com.vincent.course02;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
@@ -12,32 +11,20 @@ import org.apache.flink.util.Collector;
  * 使用Java API来开发Flink的实时处理应用程序
  * wc统计的数据源自socket
  */
-public class StreamingWCJava02App {
+public class StreamingWCJavaApp {
 
     public static void main(String[] args) throws Exception {
-
-        // 获取参数
-        int port;
-        try {
-            ParameterTool tool = ParameterTool.fromArgs(args);
-            port = tool.getInt("port");
-        } catch (Exception e) {
-            System.out.println("端口未设置, 使用默认端口9999");
-            port = 9999;
-        }
-
-
         // step1: 获取流处理上下文环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         // step2: 读取数据
-        DataStreamSource<String> text = env.socketTextStream("192.168.152.45", port);
+        DataStreamSource<String> text = env.socketTextStream("192.168.152.45", 9999);
         // step3: transform
         text.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
             @Override
             public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
                 String[] tokens = value.toLowerCase().split(",");
-                for (String token : tokens) {
-                    if (token.length() > 0) {
+                for(String token: tokens) {
+                    if(token.length() > 0) {
                         out.collect(new Tuple2<String, Integer>(token, 1));
                     }
                 }
