361408dad9543a3382715150d488987a62be1ebb
vincent-duan <1621424380@qq.com>
2020-04-18 10:45:54 +0800

    update

--- springboot-flink-train/src/main/java/com/vincent/course04/JavaCounterApp.java
+++ springboot-flink-train/src/main/java/com/vincent/course04/JavaCounterApp.java
@@ -8,26 +8,42 @@ import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.operators.DataSource;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.streaming.api.datastream.DataStream;
 
 public class JavaCounterApp {
     public static void main(String[] args) throws Exception {
+//        ExecutionEnvironment executionEnvironment = ExecutionEnvironment.getExecutionEnvironment();
+//        DataSource<String> data = executionEnvironment.fromElements("hadoop", "spark", "pyspark", "storm");
+//        DataSet dataSet = data.map(new RichMapFunction<String, String>() {
+//            long counter = 0L;
+//
+//            @Override
+//            public String map(String value) throws Exception {
+//                counter++;
+//                System.out.println(counter);
+//                return value;
+//            }
+//        }).setParallelism(2);
+//        dataSet.writeAsText("/Users/duandingyang/test-data/test4", FileSystem.WriteMode.OVERWRITE).setParallelism(3);
+//        JobExecutionResult javaCounterApp = executionEnvironment.execute("JavaCounterApp");
+
         ExecutionEnvironment executionEnvironment = ExecutionEnvironment.getExecutionEnvironment();
         DataSource<String> data = executionEnvironment.fromElements("hadoop", "spark", "pyspark", "storm");
-        DataSet dataSet = data.map(new RichMapFunction<String, String>() {
+        DataSet<String> text = data.map(new RichMapFunction<String, String>() {
             LongCounter counter = new LongCounter();
-
             @Override
             public void open(Configuration parameters) throws Exception {
-                getRuntimeContext().addAccumulator("ele-counts-java",counter);
+                getRuntimeContext().addAccumulator("ele-counts-java", counter);
+                System.out.println("open counter: "+counter.getLocalValue());
             }
-
             @Override
             public String map(String value) throws Exception {
                 counter.add(1);
+                System.out.println("map counter: "+counter.getLocalValue());
                 return value;
             }
-        });
-        dataSet.writeAsText("E:/test4", FileSystem.WriteMode.OVERWRITE).setParallelism(3);
+        }).setParallelism(2);
+        text.writeAsText("~/test-data/test4", FileSystem.WriteMode.OVERWRITE).setParallelism(3);
         JobExecutionResult javaCounterApp = executionEnvironment.execute("JavaCounterApp");
         long num = javaCounterApp.getAccumulatorResult("ele-counts-java");
         System.out.println("num:" + num);
